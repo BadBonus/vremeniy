486960f122b94bbe619b41bae3f2f47eff0be541
BadBonus <arturyes1993@gmail.com>
2021-02-16 22:27:39 +0300

    added styles for desktop footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,27 +3,34 @@
     <div class="Footer__content">
       <div class="Footer__textBlock">
         <b
-          >Контактный телефон по вопросам проведения Рекламной <br />
+          >Контактный телефон по вопросам проведения Рекламной
+          <br v-if="isMobileSize" />
           игры +375 (44) 583 76 93</b
         >
         <br />
         <b>Время работы:</b> с 10.00 до 18.00 понедельник-суббота <br />
-        <b>E-mail:</b> promo@nestle-cereals.by <br /><br /><b>
-          Рекламная игра «Завтракай и выигрывай!». <br />
-          Сроки проведения рекламной игры</b
-        >
-        <br />(с учетом выдачи призов и публикации результатов): <br />
-        1 марта 2021 г. – 22 мая 2021 г. <br />
-        Свидетельство о государственной регистрации №3932 от <br />
-        02.02.2021 г., <br />
-        выданное Министерством антимонопольного регулирования <br />
-        и торговли Республики Беларусь. <br />
-        <b>Организатор:</b> ООО «Проспектпресс», УНП 101520868 <br />
-        <br />
-        <b>
-          ®Владелец товарных знаков: <br />
-          Société des Produits Nestlé S.A. (Швейцария)</b
-        >
+        <b>E-mail:</b> promo@nestle-cereals.by <br /><br />
+        <div class="Footer__lastPartOfText">
+          <b>
+            Рекламная игра «Завтракай и выигрывай!». <br />
+            Сроки проведения рекламной игры</b
+          >
+          <br v-if="isMobileSize" />
+          (с учетом выдачи призов и публикации результатов): <br />
+          1 марта 2021 г. – 22 мая 2021 г. <br />
+          Свидетельство о государственной регистрации №3932 от
+          <br v-if="isMobileSize" />
+          02.02.2021 г., <br />
+          выданное Министерством антимонопольного регулирования
+          <br v-if="isMobileSize" />
+          и торговли Республики Беларусь. <br />
+          <b>Организатор:</b> ООО «Проспектпресс», УНП 101520868 <br />
+          <br v-if="isMobileSize" />
+          <b>
+            ®Владелец товарных знаков: <br />
+            Société des Produits Nestlé S.A. (Швейцария)</b
+          >
+        </div>
       </div>
       <img src="../assets/footer_boxes.png" alt="footer_boxes" />
       <div class="Footer__linksToDocs">
@@ -47,6 +54,11 @@ export default {
       isOpenWinners: false,
     };
   },
+  computed: {
+    isMobileSize() {
+      return this.$vssWidth < this.$desktopSize;
+    },
+  },
 };
 </script>
 
@@ -67,6 +79,13 @@ export default {
   background-position: bottom;
   background-size: contain;
 
+  @media @desktop {
+    font-size: 1.63vw;
+    line-height: 1.8vw;
+    .bcgImage("../assets/footerDecor.png");
+    padding-bottom: 1.2vw;
+  }
+
   img {
     width: 94vw;
     display: block;
@@ -75,11 +94,24 @@ export default {
   }
   &__content {
     margin-top: 11.1vw;
+    @media @desktop {
+      margin-top: 3.4vw;
+    }
   }
   img {
     margin-top: 10.1vw;
+    @media @desktop {
+      margin-top: 1.5vw;
+      width: 81.3vw;
+      margin-bottom: 0.4vw;
+    }
   }
   &__linksToDocs {
+    @media @desktop {
+      display: flex;
+      justify-content: flex-start;
+      padding-left: 20.3vw;
+    }
     a {
       color: @main_color2;
       display: block;
@@ -88,7 +120,21 @@ export default {
       font-size: 3.2vw;
       &:first-child {
         margin-bottom: 3vw;
+        @media @desktop {
+          margin-right: 10.5vw;
+          margin-bottom: 0;
+        }
       }
+      @media @desktop {
+        font-size: 1.25vw;
+        margin-bottom: 0;
+      }
+    }
+  }
+
+  &__lastPartOfText {
+    @media @desktop {
+      font-size: 1.24vw;
     }
   }
 }
